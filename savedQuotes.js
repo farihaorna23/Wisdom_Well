569296d32cb61ae9db6c8c5a3681bab7e975a69f
Fariha <farihaorna@gmail.com>
2024-11-10 18:07:15 -0500

    retrived all the saved quotes from the local storage and displaying it on the page with some css style

--- savedQuotes.js
+++ savedQuotes.js
@@ -1,4 +1,19 @@
-import {myQuote} from "./script.js";
+//DOM Element
+const savedQuotesContainer = document.querySelector('.saved-quotes-container')
+const savedQuote = document.querySelector(".saved-quote")
+//Variable
+let myQuotes = JSON.parse(localStorage.getItem("savedQuotes")) //getting all the quotes saved in local storage
 
-const quoteContainer = document.querySelector('.quote-container')
-console.log(myQuote)
+
+//creating the quotes and displaying it on the page with some css style
+if (myQuotes.length > 0){
+  myQuotes.forEach((quote) => {
+    let quoteContainer = document.createElement("div")
+    quoteContainer.classList.add("saved-quote")
+    let textParagraph = document.createElement("p")
+    textParagraph.textContent = quote;
+    quoteContainer.appendChild(textParagraph)
+    savedQuotesContainer.appendChild(quoteContainer)
+  })
+
+}
