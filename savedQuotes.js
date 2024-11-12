38f7dd33ce23e1330be8daaf6f8f15004ea7310d
Fariha <farihaorna@gmail.com>
2024-11-11 21:32:28 -0500

    added the delete icon to each of the saved quotes

--- savedQuotes.js
+++ savedQuotes.js
@@ -12,8 +12,13 @@ if (myQuotes.length > 0){
     quoteContainer.classList.add("saved-quote")
     let textParagraph = document.createElement("p")
     textParagraph.textContent = quote;
+    let deleteIcon = document.createElement("i")
+    deleteIcon.classList.add("fa-solid", "fa-x","delete-icon")
     quoteContainer.appendChild(textParagraph)
+    quoteContainer.appendChild(deleteIcon)
     savedQuotesContainer.appendChild(quoteContainer)
   })
 
 }
+
+
